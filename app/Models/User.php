a8e26909d470c217c6410939983e3934c632121e
NasrunH <162709427+NasrunH@users.noreply.github.com>
2026-01-07 19:34:53 +0700

    Revert "Merge pull request #1 from ocaw12/api"
    This reverts commit 76ac1fa6ea099a85aba49a29da3fe4dff63d73e9, reversing
    changes made to f360af96af3ad63b68d4460d23e79c6c45b84f8b.

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo; // Tambahkan ini
 use Illuminate\Database\Eloquent\Relations\HasOne; // Tambahkan ini
-use Laravel\Sanctum\HasApiTokens;
 
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
